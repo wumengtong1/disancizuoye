2972106481896fbdcbd03059a9d0b07ef124583d
wumengtong1 <50296051+wumengtong1@users.noreply.github.com>
2020-05-19 00:59:56 +0800

    first commit

--- SMTest.java
+++ SMTest.java
@@ -3,7 +3,7 @@ package wumengtong;//测试类
 public class SMTest {
     public static void main(String[] args) {
         StudentManager studentManager = new StudentManager();//创建学生管理类对象studentManager
-        studentManager.addStudent(1,"吴孟桐","1999.7",true);//插入我的信息
+        studentManager.addStudent(1,"吴孟桐","1999.7.18",true);//插入我的信息
         studentManager.seek("吴孟桐");
         studentManager. change("吴孟桐");
         studentManager.printAll();
